de59f781fd8f4bd4ad1b6b0bcfe17a6492c7a6fe
Your Name <chafiaadssi42@gmail.com>
2024-06-26 21:58:31 +0200

    message à mettre en dynamic

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,6 +9,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransportFactory;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransportFactoryInterface;
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -31,15 +36,11 @@ class ContactController extends AbstractController
                 ->from($contactFormData['email'])
                 ->to($contactFormData['email'])
                 ->subject($contactFormData['subject'])
-                ->text($contactFormData['message'])
-                ->html('<p>' . $contactFormData['message'] . '</p>');
+                ->text($contactFormData['message']);
+                // ->html('<p>' . $contactFormData['message'] . '</p>');
 
             $mailer->send($message);
             $this->addFlash('success', 'Ton message a bien été envoyé !');
-
-            return $this->redirectToRoute('app_contact');
-
-
             
         } else {
             $this->addFlash('error', 'Ton message n\'a pas pu être envoyé !');
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -11,12 +11,36 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(UserRepository $userPicture): Response
+    public function profil(): Response
     {
-        $userPicture = $userPicture->findAll();
-        
-        return $this->render('profil/profil.html.twig', [
-           
-        ]);
+        return $this->render('profil/profil.html.twig');
+    }
+
+    // #[Route('/profil/{id}', name: 'app_profil_id')]
+    // public function profil($id, UserRepository $userRepository, PictureRepository $pictureRepository): Response
+    // {
+    //     $user = $userRepository->find($id);
+    //     $pictures = $pictureRepository->findBy(['user' => $user]);
+
+    //     return $this->render('profil/profil.html.twig', [
+    //         'user' => $user,
+    //         'pictures' => $pictures
+    //     ]);
+    // }
+
+    #[Route('/profil/annonce', name: 'app_profile_announcement')]
+    public function announcement(): Response
+    {
+        return $this->render('profil/anouncemment.html.twig');
+    }
+
+    #[Route('/profil/favoris', name: 'app_profil_favorite')]
+    public function favorite(): Response
+    {
+        return $this->render('profil/favorite.html.twig');
     }
 }
+
+    
+    
+
